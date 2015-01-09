629e7d85665ae55065bd37740bf3c296047fe232
edesignerbrasil <charles.eduardo.br@hotmail.com>
2015-01-09 15:34:28 -0200

    4.5 - Reinstalando o servidor web 04 Declarando recursos tipo file

--- manifests/web.pp
+++ manifests/web.pp
@@ -6,3 +6,27 @@ package { ["mysql-client", "tomcat7"]:
 	ensure => installed,
 	require => Exec["apt-update"],
 }
+
+file { "/var/lib/tomcat7/conf/.keystore":
+	owner => root,
+	group => tomcat7,
+	mode => 0640,
+	source => "/vagrant/manifests/.keystore",
+	require => Package["tomcat7"],
+}
+
+file { "/var/lib/tomcat7/conf/server.xml":
+	owner => root,
+	group => tomcat7,
+	mode => 0644,
+	source => "/vagrant/manifests/server.xml",
+	require => Package["tomcat7"],
+}
+
+file { "/etc/default/tomcat7":
+	owner => root,
+	group => root,
+	mode => 0644,
+	source => "/vagrant/manifests/tomcat7",
+	require => Package["tomcat7"],
+}
