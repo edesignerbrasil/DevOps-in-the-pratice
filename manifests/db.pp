7f1ff4e231e061cb2466bb335fc9d2a2b518c553
root <root@edesignerbrasil.edesignerbrasil.com.br>
2015-01-10 19:36:56 -0200

    5.1 - Classes e tipos definidos, refatorando tipos definidos

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,54 +1,60 @@
 class mysql-server {
 
-exec { "apt-update":
-	command => "/usr/bin/apt-get update"
+	exec { "apt-update":
+		command => "/usr/bin/apt-get update"
+	}
+
+	package { "mysql-server":
+		ensure => installed,
+		require => Exec["apt-update"],
+	}
+
+	file { "/etc/mysql/conf.d/allow_external.cnf":
+		owner => mysql,
+		group => mysql,
+		mode => 0644,
+		content => template("/vagrant/mainfests/allow_ext.cnf"),
+		require => Package["mysql-server"],
+		notify => Service["mysql"],
+	}
+
+	service { "mysql":
+		ensure => running,
+		enable => true,
+		hasstatus => true,
+		hasrestart => true,
+		require => Package["mysql-server"],
+	}
+
+	exec { "remove-anonymous-user":
+		command => "mysql -u root -e \"DELETE FROM mysql.user WHERE user=''; FLUSH PRIVILEGES\"",
+		onlyif => "mysql -u ' '",
+		path => "/usr/bin",
+		require => Service["mysql"],
+	}
 }
 
-package { "mysql-server":
-	ensure => installed,
-	require => Exec["apt-update"],
-}
+define mysql-db($schema, $user = $title, $password) {
+	
+	Class['mysql-server'] -> Mysql-db[$title]
 
-file { "/etc/mysql/conf.d/allow_external.cnf":
-	owner => mysql,
-	group => mysql,
-	mode => 0644,
-	content => template("/vagrant/mainfests/allow_ext.cnf"),
-	require => Package["mysql-server"],
-	notify => Service["mysql"],
-}
+		exec { "$title-schema":
+			unless => "mysql -u root $schema",
+			command => "mysqladmin -u root create $schema",
+			path => "/usr/bin/",
+		}
 
-service { "mysql":
-	ensure => running,
-	enable => true,
-	hasstatus => true,
-	hasrestart => true,
-	require => Package["mysql-server"],
-}
-
-exec { "remove-anonymous-user":
-	command => "mysql -u root -e \"DELETE FROM mysql.user WHERE user=’’; FLUSH PRIVILEGES\"",
-	onlyif => "mysql -u’ ’",
-	path => "/usr/bin",
-	require => Service["mysql"],
-}
-
-
+		exec { "$title-user":
+			unless => "mysql -u $user -p$password $schema",
+			command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON $schema.* TO '$user'@'%' IDENTIFIED BY '$password';\"",
+			path => "/usr/bin/",
+			require => Exec["$title-schema"],
+		}
 }
 
 include mysql-server
 
-exec { "loja-schema":
-	unless => "mysql -u root loja_schema",
-	command => "mysqladmin -uroot create loja_schema",
-	path => "/usr/bin/",
-	require => Service["mysql"],
-}
-
-
-exec { "loja-user":
-	unless => "mysql -u loja -p lojasecret loja_schema",
-	command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON loja_schema.* TO 'loja'@'%'' IDENTIFIED BY 'lojasecret';\"",
-	path => "/usr/bin/",
-	require => Exec["loja-schema"],
-}
+	mysql-db { "loja":
+		schema => "loja_schema",
+		password => "lojasecret",
+	}
